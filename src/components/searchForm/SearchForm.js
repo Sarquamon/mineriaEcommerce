a06d4d3d4635eafee54f642a3b422e445d0f2afd
sarquamon <sarquamon@gmail.com>
2019-12-13 00:49:31 -0600

    Fixed search field to work with new API and fixed its overlaping issue with navbar

--- src/components/searchForm/SearchForm.js
+++ src/components/searchForm/SearchForm.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 const SearchForm = props => {
-  return (
-    <div className="d-flex justify-content-around">
-      <form className="form-inline" onSubmit={props.getProduct}>
-        <div className="btn-toolbar" role="toolbar">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Ingrese un articulo"
-              name="productName"
-            />
-          </div>
+	return (
+		<div className="d-flex justify-content-around">
+			<form className="form-inline" onSubmit={props.getProduct}>
+				<div className="btn-toolbar" role="toolbar">
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control custom-form-control"
+							placeholder="Ingrese un articulo"
+							name="productName"
+						/>
+					</div>
 
-          <div className="btn-group ml-2">
-            <button className="btn btn-outline-primary">Buscar</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+					<div className="btn-group ml-2">
+						<button className="btn btn-outline-primary">Buscar</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default SearchForm;
