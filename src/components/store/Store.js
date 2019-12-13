6b665cec1083a178a9ccb035bd6c94e8bb07d03d
sarquamon <sarquamon@gmail.com>
2019-12-13 00:46:25 -0600

    Fixed bugs with getting product img and data

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -6,72 +6,75 @@ import ReactLoading from "react-loading";
 import SearchForm from "../searchForm/SearchForm";
 import Products from "../products/Products";
 
-const vetAPI = "https://newvetsapi.herokuapp.com";
+const vetAPI = "http://localhost:2000";
 
 const Store = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  const getAllItems = async () => {
-    const response = await axios.get(`${vetAPI}/all/product/`);
+	const getAllItems = async () => {
+		const response = await axios.get(`${vetAPI}/product`);
+		// console.log(response.data.products[1].productImage);
 
-    setProducts(response.data);
-    setLoading(false);
-  };
+		setProducts(response.data.products);
+		setLoading(false);
+	};
 
-  useEffect(() => {
-    const json = sessionStorage.getItem("products");
-    const products = JSON.parse(json);
-    setProducts({products});
+	useEffect(() => {
+		const json = sessionStorage.getItem("products");
+		const products = JSON.parse(json);
+		setProducts({products});
 
-    getAllItems();
-    console.log("corri we");
-  }, []);
+		getAllItems();
+		// console.log("corri we");
+	}, []);
 
-  useEffect(() => {
-    const newProducts = JSON.stringify(products);
-    sessionStorage.setItem("products", newProducts);
-  }, [products]);
+	useEffect(() => {
+		const newProducts = JSON.stringify(products);
+		sessionStorage.setItem("products", newProducts);
+	}, [products]);
 
-  const getProduct = e => {
-    const productName = e.target.elements.productName.value;
+	const getProduct = e => {
+		const productName = e.target.elements.productName.value;
 
-    e.preventDefault();
+		e.preventDefault();
 
-    axios
-      .get(`${vetAPI}/product/name/${productName}`)
-      .then(res => {
-        setProducts(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+		axios
+			.get(`${vetAPI}/product/searchName/${productName}`)
+			.then(res => {
+				// console.log(res.data);
 
-  return (
-    <div>
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-lg-3"></div>
-          <div className="col-lg-6">
-            <SearchForm getProduct={getProduct} />
-          </div>
-          <div className="col-lg-3"></div>
-        </div>
-        <div className="row">
-          <div className="col-lg-12 justify-content-around">
-            {loading ? (
-              <div style={{marginLeft: 8 + "rem"}} className="text-center">
-                <ReactLoading type={"balls"} color={"#000000"} width={790} />
-              </div>
-            ) : (
-              <Products products={products} />
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				setProducts(res.data.products);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<div>
+			<div className="container mt-3">
+				<div className="row">
+					<div className="col-lg-3"></div>
+					<div className="col-lg-6">
+						<SearchForm getProduct={getProduct} />
+					</div>
+					<div className="col-lg-3"></div>
+				</div>
+				<div className="row">
+					<div className="col-lg-12 justify-content-around">
+						{loading ? (
+							<div style={{marginLeft: 8 + "rem"}} className="text-center">
+								<ReactLoading type={"balls"} color={"#000000"} width={790} />
+							</div>
+						) : (
+							<Products products={products} />
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Store;
