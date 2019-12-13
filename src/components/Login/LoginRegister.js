70f0c70081c34f971e2e7ff3309969fa5c828dac
sarquamon <sarquamon@gmail.com>
2019-12-13 00:47:35 -0600

    Added a login/register form

--- src/components/Login/LoginRegister.js
+++ src/components/Login/LoginRegister.js
@@ -6,7 +6,7 @@ import "./LoginRegister.css";
 import RegisterForm from "./RegisterForm/RegisterForm";
 import LoginForm from "./LoginForm/LoginForm";
 
-const vetAPI = "https://newvetsapi.herokuapp.com";
+const vetAPI = "http://localhost:2000/";
 
 const checkEmail = async useremail => {
 	const result = await axios.get(`${vetAPI}/user/email/${useremail}`);
@@ -14,26 +14,66 @@ const checkEmail = async useremail => {
 	return result.data.userEmail;
 };
 
-const register = e => {
-	// const username = e.target.elements.username.value;
-	// const userlastname = e.target.elements.userlastname.value;
-	// const useremail = e.target.elements.useremail.value;
-	// const userpwd = e.target.elements.userpwd.value;
-	// const userpwd2 = e.target.elements.userpwd2.value;
+const checkPwd = async (useremail, userPwd) => {
+	const result = await axios.get(`${vetAPI}/user/email/${useremail}`);
+
+	return result.data.userPwd;
+};
+
+const createUser = async (username, userlastname, useremail, userpwd) => {
+	axios
+		.post(`${vetAPI}/create/user/`, {
+			userEmail: useremail,
+			username: username,
+			userPwd: userpwd,
+			firstName: username,
+			surName: userlastname,
+			address: "Corfu 72",
+			isVet: true,
+			activeUser: true
+		})
+		.then(res => console.log(`exito! ${res.data}`))
+		.catch(err => console.log(`error! ${err}`));
+};
+
+const register = async e => {
+	const username = e.target.elements.username.value;
+	const userlastname = e.target.elements.userlastname.value;
+	const useremail = e.target.elements.useremail.value;
+	const userpwd = e.target.elements.userpwd.value;
+	const userpwd2 = e.target.elements.userpwd2.value;
 
 	e.preventDefault();
+
+	if (userpwd === userpwd2) {
+		const emailExists = await checkEmail(useremail);
+		// console.log(emailExists);
+		if (!emailExists) {
+			createUser(username, userlastname, useremail, userpwd);
+		} else {
+			console.log(`Existe`);
+		}
+	} else {
+		console.log(`Contraseñas no son iguales`);
+	}
 };
 
 const login = async e => {
 	const useremail = e.target.elements.useremail.value;
-	// const userpwd = e.target.elements.userpwd.value;
+	const userpwd = e.target.elements.userpwd.value;
 
 	e.preventDefault();
 
-	const available = await checkEmail(useremail);
-	console.log(available);
-	if (available) {
-		console.log(`Existe`);
+	const userExists = await checkEmail(useremail);
+	console.log(userExists);
+	if (userExists) {
+		console.log(`Usuario ya existe`);
+		const rightpwd = await checkPwd(useremail, userpwd);
+		if (rightpwd) {
+			console.log(`correcta`);
+		} else {
+			console.log(`incorrecta`);
+		}
 	} else {
 		console.log(`no existe`);
 	}
@@ -45,12 +85,12 @@ export const LoginRegister = () => {
 
 	const changeView = buttonId => {
 		if (buttonId === "loginBtn") {
-			console.log("Login");
+			// console.log("Login");
 
 			setregisterBtn("invisible");
 			setloginBtn("visible");
 		} else {
-			console.log("Register");
+			// console.log("Register");
 			setloginBtn("invisible");
 			setregisterBtn("visible");
 		}
@@ -90,54 +130,6 @@ export const LoginRegister = () => {
 
 export default LoginRegister;
 /*
-
-$(document).ready(function() {
-  $(".first-button").on("click", function() {
-    $(".animated-icon1").toggleClass("open");
-  });
-  $(".second-button").on("click", function() {
-    $(".animated-icon2").toggleClass("open");
-  });
-  $(".third-button").on("click", function() {
-    $(".animated-icon3").toggleClass("open");
-  });
-});
-
-let emailArray = ["sarquamon@gmail.com"];
-let passwordArray = ["sa"];
-
-function login() {
-  event.preventDefault();
-
-  let email = document.getElementById("userEmailLog").value;
-  let password = document.getElementById("userPassLog").value;
-
-  let i = emailArray.indexOf(email);
-
-  if (emailArray.indexOf(email) == -1) {
-    if (email == "") {
-      alert("Email required.");
-      return;
-    }
-    alert("Email does not exist.");
-    return;
-  } else if (passwordArray[i] != password) {
-    if (password == "") {
-      alert("Password required.");
-      return;
-    }
-    alert("Password does not match.");
-    return;
-  } else {
-    alert(email + " yor are login Now");
-
-    document.getElementById("userEmailLog").value = "";
-    document.getElementById("userPassLog").value = "";
-    //window.location.to = "buttonAlert.html";
-    return;
-  }
-}
-
 function forgot() {
   event.preventDefault();
 
@@ -155,48 +147,4 @@ function forgot() {
   alert("email is send to your email check it in 24hr. \n Thanks");
   document.getElementById("userEmailLog").value = "";
 }
-
-function register() {
-  event.preventDefault();
-
-  let name = document.getElementById("userName");
-  let lastName = document.getElementById("userLastName");
-  let email = document.getElementById("emailUserRe").value;
-  let password = document.getElementById("pwdUserReg").value;
-  let passwordRetype = document.getElementById("pwd2UserReg").value;
-
-  if (name == "") {
-    alert("Name required.");
-    return;
-  } else if (lastName == "") {
-    alert("Last name required.");
-    return;
-  } else if (email == "") {
-    alert("Email required.");
-    return;
-  } else if (password == "") {
-    alert("Password required.");
-    return;
-  } else if (passwordRetype == "") {
-    alert("Password required.");
-    return;
-  } else if (password != passwordRetype) {
-    alert("Password don't match retype your Password.");
-    return;
-  } else if (emailArray.indexOf(email) == -1) {
-    emailArray.push(email);
-    passwordArray.push(password);
-
-    alert(email + "  Thanks for registration. \nTry to login Now");
-
-    document.getElementById("emailUserRe").value = "";
-    document.getElementById("pwdUserReg").value = "";
-    document.getElementById("pwd2UserReg").value = "";
-  } else {
-    alert(email + " is already register.");
-    return;
-  }
-}
-
-
 */
